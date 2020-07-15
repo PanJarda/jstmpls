d2b2606b226e5a96ff73ab1dca933d697cb1f2c7
jardapernik <jaroslav.pernica9@gmail.com>
2020-07-16 01:26:06 +0200

    oop format

--- index.oop.js
+++ index.oop.js
@@ -1,197 +1,197 @@
-( function( global, factory ) {
-	"use strict";
-	
-	if ( typeof module === "object" && typeof module.exports === "object" ) {
-		module.exports = factory( global, true );
-	} else {
-		factory( global );
-	}
-} )( typeof window !== "undefined" ? window : this, function( window, global ) {
+(function (global, factory) {
     "use strict";
 
-    function H( tagName, props, children ) {
-        var _      = this;
-        _.puns     = [];
-        _.ref      = document.createElement( tagName );
-        _.children = children;
-        
-        for ( var attr in props )
-            _.setAttribute( attr, props[ attr ] );
+    if (typeof module === "object" && typeof module.exports === "object") {
+        module.exports = factory(global, true);
+    } else {
+        factory(global);
+    }
+})(typeof window !== "undefined" ? window : this, function (window, global) {
+    "use strict";
 
-        switch ( typeof children ) {
+    function H(tagName, props, children) {
+        var _ = this;
+        _.puns = [];
+        _.ref = document.createElement(tagName);
+        _.children = children;
+
+        for (var attr in props)
+            _.setAttribute(attr, props[attr]);
+
+        switch (typeof children) {
             case "string":
-                _.ref.appendChild( document.createTextNode( children ) );
+                _.ref.appendChild(document.createTextNode(children));
                 break;
             case "function":
                 var txt = document.createTextNode('');
-                _.ref.appendChild( txt );
+                _.ref.appendChild(txt);
                 _.puns.push({ fn: children, node: txt });
                 break;
             case "object": // array
                 var N = children.length;
-                for ( var i = 0; i < N; i++ )
-                    _.ref.appendChild( children[ i ].ref );
+                for (var i = 0; i < N; i++)
+                    _.ref.appendChild(children[i].ref);
         }
     }
 
     H.prototype = {
-        setAttribute: function( attr, value ) {
+        setAttribute: function (attr, value) {
             var _ = this;
-            
-            if ( attr === "ev" ) {
-                for ( var evName in value )
-                    _.ref.addEventListener( evName, value[ evName ] );
+
+            if (attr === "ev") {
+                for (var evName in value)
+                    _.ref.addEventListener(evName, value[evName]);
                 return;
             }
-            
-            if ( typeof value === "function" ) {
-                var node = document.createAttribute( attr );
-        
+
+            if (typeof value === "function") {
+                var node = document.createAttribute(attr);
+
                 _.puns.push({
-                    fn:   value,
+                    fn: value,
                     node: node
                 });
-                
-                _.ref.setAttributeNode( node );
+
+                _.ref.setAttributeNode(node);
 
                 return;
             }
-        
-            _.ref.setAttribute( attr, value );
+
+            _.ref.setAttribute(attr, value);
         },
-        update: function( model ) {
+        update: function (model) {
             var _ = this,
                 i = 0,
                 N = _.puns.length,
                 node, pun, newVal;
-            
-            for ( i = 0; i < N; i++ ) {
-                pun = _.puns[ i ];
+
+            for (i = 0; i < N; i++) {
+                pun = _.puns[i];
                 node = pun.node;
-                newVal = pun.fn( model );
-            
-                if ( pun.old === newVal )
+                newVal = pun.fn(model);
+
+                if (pun.old === newVal)
                     return;
-        
-                if ( typeof node === "object" ) {
-                    if ( _.ref.tagName === "INPUT" &&
+
+                if (typeof node === "object") {
+                    if (_.ref.tagName === "INPUT" &&
                         node.name === "value" &&
-                        _.ref.value !== newVal )
+                        _.ref.value !== newVal)
                         _.ref.value = newVal;
                     else
                         node.nodeValue = newVal;
                 } else {
-                    _.ref.setAttribute( node, newVal );
+                    _.ref.setAttribute(node, newVal);
                 }
-        
+
                 pun.old = newVal;
             }
 
-            if ( typeof _.children === "object" )
-                for ( i in _.children )
-                    _.children[ i ].update( model );
+            if (typeof _.children === "object")
+                for (i in _.children)
+                    _.children[i].update(model);
         }
     };
 
-    function HIf( parent, cond, child ) {
-        var _      = this;
-        _.parent   = parent;
+    function HIf(parent, cond, child) {
+        var _ = this;
+        _.parent = parent;
         _.appended = false;
-        _.ref      = parent.ref;
-        _.cond     = cond;
-        _.child    = child;
-        _.ref      = parent.ref;
+        _.ref = parent.ref;
+        _.cond = cond;
+        _.child = child;
+        _.ref = parent.ref;
     }
 
-    HIf.prototype.update = function( model ) {
+    HIf.prototype.update = function (model) {
         var _ = this,
-        child = _.child,
-        parent = _.parent,
-        res = _.cond( model );
+            child = _.child,
+            parent = _.parent,
+            res = _.cond(model);
 
-        parent.update( model );
-        
-        if ( res )
-            child.update( model );
+        parent.update(model);
 
-        if ( _.appended && !res ) {
-            parent.ref.removeChild( child.ref );
+        if (res)
+            child.update(model);
+
+        if (_.appended && !res) {
+            parent.ref.removeChild(child.ref);
             _.appended = false;
-        } else if ( res ) {
-            parent.ref.appendChild( child.ref );
+        } else if (res) {
+            parent.ref.appendChild(child.ref);
             _.appended = true;
         }
     };
 
-    function HFor( parent, arrFn, childFactory ) {
-        var _          = this;
-        _.old          = [];
-        _.children     = [];
-        _.parent       = parent;
-        _.arrFn        = arrFn;
+    function HFor(parent, arrFn, childFactory) {
+        var _ = this;
+        _.old = [];
+        _.children = [];
+        _.parent = parent;
+        _.arrFn = arrFn;
         _.childFactory = childFactory;
-        _.ref          = parent.ref;
+        _.ref = parent.ref;
     }
 
-    HFor.prototype.update = function( model ) {
+    HFor.prototype.update = function (model) {
         var _ = this;
-        _.parent.update( model );
-        
-        var arr = _.arrFn( model ),
+        _.parent.update(model);
+
+        var arr = _.arrFn(model),
             child,
             item,
             N = arr.length;
-        for ( var i = 0; i < N; i++ ) {
-            item = arr[ i ];
+        for (var i = 0; i < N; i++) {
+            item = arr[i];
             // TODO: deepcompare
-            if ( _.old[ i ] === item ) 
+            if (_.old[i] === item)
                 continue;
-            
-            if ( _.children[ i ] ) {
-                _.children[ i ].update({ index: i, item: item });
+
+            if (_.children[i]) {
+                _.children[i].update({ index: i, item: item });
                 continue;
             }
-            
+
             child = _.childFactory();
             child.update({ index: i, item: item });
-            _.children.push( child );
-            _.parent.ref.appendChild( child.ref );
+            _.children.push(child);
+            _.parent.ref.appendChild(child.ref);
         }
 
         // odrizneme precuhujici prvky
         var oN = _.old.length,
             newN = arr.length;
-        if ( oN > newN ) {
+        if (oN > newN) {
             i = oN - newN;
-            _.children.splice( newN );
-            while ( i-- )
-                _.parent.ref.removeChild( _.parent.ref.children[ newN ] );
+            _.children.splice(newN);
+            while (i--)
+                _.parent.ref.removeChild(_.parent.ref.children[newN]);
         }
 
         _.old = arr.slice();
     };
 
-    function h( tagName, props, children ) {
-        return new H( tagName, props, children );
+    function h(tagName, props, children) {
+        return new H(tagName, props, children);
     }
 
-    function hIf( parent, cond, child ) {
-        return new HIf( parent, cond, child );
+    function hIf(parent, cond, child) {
+        return new HIf(parent, cond, child);
     }
 
-    function hFor( parent, arrFn, childFactory ) {
-        return new HFor( parent, arrFn, childFactory );
+    function hFor(parent, arrFn, childFactory) {
+        return new HFor(parent, arrFn, childFactory);
     }
 
-	if ( !global ) {
-        window.h    = h;
-        window.hIf  = hIf;
+    if (!global) {
+        window.h = h;
+        window.hIf = hIf;
         window.hFor = hFor;
     }
 
     return {
-        h:    h,
-        hIf:  hIf,
+        h: h,
+        hIf: hIf,
         hFor: hFor
     };
 });
