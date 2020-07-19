cf41d7a8ce71a5529113e45b06126e832f849cf7
jardapernik <jaroslav.pernica9@gmail.com>
2020-07-19 19:21:30 +0200

    ffds

--- test.js
+++ test.js
@@ -1,98 +1,122 @@
-var Model = {
-    state: {
-        clicks: 0,
-        error: "",
-        className: "",
-        show: true,
-        input: "",
-        itemInputs: [1,2,3,4,5],
-        items: [
-            1,2,3,4,5
-        ]
-    },
-
-}
-
 /*
 asynchronicita nehraje roli since vsecko je singlethreaded
  */
+/*
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function() {
+    if (xhr.readyState !== XMLHttpRequest.DONE)
+        return;
+
+    var status = xhr.status;
+    if (status === 0 || (status >= 200 && status < 400)) {
+        console.log(this.responseText);
+    }
+};
+xhr.open("get", "/test.json");
+xhr.send("foo=bar&lorem=ipsum");
+*/
+function Dispatcher(listeners, model, view) {
+    this.listeners = listeners || {};
+    this.model = model;
+    this.view = view;
+    this.dispatch('_init');
+}
+
+Dispatcher.prototype.addEventListener = function(ev, callback) {
+    (this.listeners = this.listeners[ ev ] || []).push(callback);
+}
+
+Dispatcher.prototype.dispatch = function(action, e) {
+    if (action !== '_before' && action !== '_after' && action !== '_init')
+        this.dispatch('_before', e);
+
+    var lst = this.listeners[action];
+    if (lst) {
+        var i = lst.length;
+        while (i--) {
+            this.model[lst[i]](e);
+        }
+    }
+    if (action !== '_before' && action !== '_after' && action !== '_init')
+        this.dispatch('_after', e);
+
+    if (action === '_after' || action === '_init') {
+        var self = this;
+        window.requestAnimationFrame(function() {
+            self.view.update(self.model);
+        });
+    }
+};
+
 function main() {
-    var init = {
+    var model = {
         clicks: 0,
         error: "",
         className: "",
         show: true,
         input: "",
         itemInputs: [],
-        items: []
-    };
-
-    var model = init;
-    function loadModel() {
-        console.log('loading model');
-        model = JSON.parse(window.localStorage.getItem("model"));
-        updateView();
-    }
-    setTimeout(loadModel, 0);
-     
-    function addClicks( event ) {
-        model.clicks++;
-        if ( model.clicks > 0 ) {
-            model.error = "";
-            model.show = true;
+        items: [],
+        active: [],
+        completed: [],
+        addClicks: function() {
+            this.clicks++;
+            if ( this.clicks > 0 ) {
+                this.error = "";
+                this.show = true;
+            }
+        },
+        subClicks: function() {
+            this.clicks--;
+            if ( this.clicks < 0 ) {
+                this.error = "Fok You!";
+                this.show = false;
+            }
+        },
+        toggleClass: function() {
+            this.className = this.className === "red" ? "" : "red";
+        },
+        inputChange: function(e) {
+            this.input = e.target.value;
+        },   
+        addItem: function() {
+            this.items.push(this.input);
+            this.itemInputs.push(this.input);
+            this.input = "";
+        },
+        removeItem: function(event) {
+            console.log("removing item " + event.target.getAttribute("data-index"));
+            var id;
+            this.items.splice(id = event.target.getAttribute("data-index") * 1, 1);
+            this.itemInputs.splice(id);
+        },
+        editItem: function(e) {
+            var i = e.target.getAttribute("data-index");
+            this.items[i] = this.itemInputs[ i ];
+            this.itemInputs[ i ] = "";
+        },
+        itemInputChange: function( event ) {
+            var index = event.target.getAttribute("data-index");
+            this.itemInputs[index] = event.target.value;
+        },
+        storeModel: function() {
+            console.log('storing');
+            var self = this;
+            if (window.localStorage)
+                setTimeout(function() {
+                    window.localStorage.setItem("model", JSON.stringify( self ));
+                },0);
+        },
+        loadModel: function() {
+            console.log('loading');
+            var model = JSON.parse(window.localStorage.getItem("model"));
+            var self = this;
+            if (model) {
+                Object.assign(self, model);
+            }
         }
-        updateView();
-    }
-    
-    function subClicks( event ) {
-        model.clicks--;
-        if ( model.clicks < 0 ) {
-            model.error = "Fok You!";
-            model.show = false;
-        }
-        updateView();
-    }
-    
-    function toggleClass( event ) {
-        model.className = model.className === "red" ? "" : "red";
-        updateView();
-    }
-    
-    function inputChange(e) {
-        model.input = e.target.value;
-    }
-    
-    function addItem(event) {
-        model.items.push(model.input);
-        model.itemInputs.push(model.input);
-        model.input = "";
-        updateView();
-    }
-    
-    function removeItem( event ) {
-        console.log("removing item " + event.target.getAttribute("data-index"));
-        var id;
-        model.items.splice(id = event.target.getAttribute("data-index") * 1, 1);
-        model.itemInputs.splice(id);
-        updateView();
-    }
-    
-    function itemInputChange( event ) {
-        var index = event.target.getAttribute("data-index");
-        model.itemInputs[index] = event.target.value;
-    }
-    
-    function editItem(e) {
-        var i = e.target.getAttribute("data-index");
-        model.items[i] = model.itemInputs[ i ];
-        model.itemInputs[ i ] = "";
-        updateView();
     }
 
-    function storeModel() {
-        window.localStorage && window.localStorage.setItem("model", JSON.stringify( model ));
-    }
-    
     var _ = false;
     
     function get( key ) {
@@ -101,34 +125,35 @@ function main() {
         }
     }
 
-    function route( e ) {
-        //console.log( e.target.href );
-        //history.pushState({}, 'prdel', e.target.href );
-        //e.preventDefault();
-    }
-
     window.addEventListener('hashchange', function() {
-        model.show = !model.show;
-        console.log(model.show);
-        updateView();
+        //model.show = !model.show;
+        //updateView();
     });
 
-    window.addEventListener('storage', loadModel)
+    window.addEventListener('storage', function() {
+        console.log('storage event');
+    })
+
+    function dispatch(msg) {
+        return function(e) {
+            dispatcher.dispatch(msg,e);
+        };
+    }
 
     var view =
         h('div', { "class": "ahoj" }, [
             h('h1', { "class": get("className"),
-                    ev: { click: toggleClass }
+                    ev: { click: dispatch('toggleClass') }
                     }, "Ahoj" ),
             h('div', _, [
-                h('p', _, get( "clicks" ) )
+                h('p', _, get('clicks'))
             ]),
             h('span', _, get( "error" ) ),
-            h('a', {href:'#prdel', ev: { click: route }}, "link1"),
+            h('a', {href:'#prdel'}, "link1"),
             h('hr'),
-            h('button', { ev: { mouseup: addClicks} }, "   +   " ),
+            h('button', { ev: { mouseup: dispatch('addClick') }}, "   +   " ),
             h('i', _, " "),
-            h('button', { ev: { mouseup: subClicks} }, "   -   " ),
+            h('button', { ev: { mouseup: dispatch('subClick')} }, "   -   " ),
             hIf( h('h2', _, "Ahoj" ),
                 get( "error" ),
                     h('span', _, get("error"))),
@@ -149,22 +174,31 @@ function main() {
                 get("items"),
                 function() {
                     return h('li', _, [
-                        h('span',_, get("item")),
-                        h('span',_, " "),
-                        h('input', {value: get("item"),"data-index": get("index"),ev:{change:itemInputChange}}),
-                        h('button',{"data-index": get("index"),ev:{click: editItem}}, "edit"),
-                        h('button', {"data-index": get("index"), ev:{ mouseup: removeItem}}, " X ")
-                    ]);
+                                h('span',_, get("item")),
+                                h('span',_, " "),
+                                h('input', {value: get("item"),"data-index": get("index"),ev:{change:dispatch('itemInputChange')}}),
+                                h('button',{"data-index": get("index"),ev:{click: dispatch('editItem')}}, "edit"),
+                                h('button', {"data-index": get("index"), ev:{ mouseup: dispatch('removeItem') }}, " X ")
+                            ]);
                 }),
-            h('input', {value: get("input"), ev:{change: inputChange }} ),
-            h('button', {ev:{mouseup: addItem}}, "add Item")
+            h('input', {value: get("input"), ev:{change: dispatch('inputChange') }} ),
+            h('button', {ev:{mouseup: dispatch('addItem')}}, "add Item")
         ]);
 
-    function updateView( m ) {
-        setTimeout(storeModel, 0);
-        view.update( m || model );
-    }
-    view.update( model );
+    var dispatcher = new Dispatcher({
+        _init:       ['loadModel'],
+        _before:     [],
+        addClick:    ['addClicks'],
+        subClick:    ['subClicks'],
+        toggleClass: ['toggleClass'],
+        inputChange: ['inputChange'],
+        addItem:     ['addItem'],
+        removeItem:  ['removeItem'],
+        itemInputChange: ['itemInputChange'],
+        editItem:    ['editItem'],
+        loadModel:   ['loadModel'],
+        _after:      ['storeModel']
+    }, model, view);
 
     var app = document.getElementById('app');
     app.innerHTML = "";
